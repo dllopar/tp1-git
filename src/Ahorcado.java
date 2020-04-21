fa32dc3440f010f41f6dd5f62dddbef2d4a9841f
RicaroD92 <duchiarg@gmail.com>
2020-04-21 15:45:13 -0300

    asdsad

--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -10,39 +10,31 @@ public class Ahorcado {
 		
 		Scanner teclado= new Scanner(System.in);
 		
-		 System.out.println("*****************************");
-			System.out.println("Ingresar su opncion:");
-			System.out.println("--------------------");
-			System.out.println("1 pasa multiplicar");
+		do{
+			System.out.println("*****************************");
+			System.out.println("Ingresar su opncion:");System.out.println("--------------------");
+			System.out.println("1 para multiplicar");
 			System.out.println("2 para restar");
-			System.out.println("3 para sumar");
+			System.out.println("3 pasa sumar");
 			System.out.println("4 2para dividir");
 			System.out.println("");
-			System.out.println("*****************************");	
-			
+			System.out.println("*****************************");
+		
+		
 		opcion = teclado.nextInt();
-		System.out.println("Ingrese el primer numero");
+			}while(opcion < 1 && opcion >4);
+		System.out.println("Ingresar el primer numero");
 		a = teclado.nextInt();
-		System.out.println("Ingrese el segundo numero");
+		System.out.println("Ingresar el segundo numero numero");
 		b = teclado.nextInt();
-	
-			switch(opcion){
-			case 1:
-				resultado= a * b;
-				break;
-				
-			case 2:
-				resultado=a-b;
-				break;
-				
-			case 3:
-				resultado=a+b;
-				break;
-				
-			case 4:
-				resultado=a/b;
-				break;
-			
+		if(opcion.equals(1)) {
+			resultado = a * b;	
+		}else if (opcion.equals(2)){
+			resultado = a + b;
+		}else if (opcion.equals(3)) {
+			resultado = a + b;
+		}else {
+			resultado = a / b;
 		}
 			System.out.println("El resultado es " + resultado.toString());
 
